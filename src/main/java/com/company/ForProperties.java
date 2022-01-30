a3cc9ebb6960e52b0f565169f669d47472c29406
Konstantin Lopatko <const42@gmail.com>
2022-01-30 13:35:16 +0300

    Refactor
    - remove unnecessary code from reading properties
    
    -rename artifact id from pom.xml
    
    - remove unnecessary lines from pom.xml

--- src/main/java/com/company/ForProperties.java
+++ src/main/java/com/company/ForProperties.java
@@ -10,19 +10,11 @@ import java.util.Properties;
 public class ForProperties {
     public static final String ARG_START = "-pathConfig=";
 
-    private static final String PROPERTY_IP_SERVER_1 = "ip_server_1";
-    private static final String PROPERTY_PORT_DO_SERVER_1 = "port_server_1";
+    private static final String PROPERTY_PORT = "port";
     private static final String PROPERTY_URL_FOR_SQL = "url_for_sql";
-    private static final String PROPERTY_IS_RASPBERRY_PI = "is_raspberry_pi";
-    private static final String PROPERTY_DEVICE_ID = "device_id";
-    private static final String PROPERTY_PERIOD_SEC = "period";
 
-    private String ip;
     private int port;
     private String urlForDb;
-    private Boolean isRpi;  // Is this client working on Raspberry Pi
-    private int deviceId;
-    private int period;  // Period sending data (in seconds)
 
     boolean load(String arg, Logger logger) {
         Properties prop = new Properties();
@@ -34,14 +26,14 @@ public class ForProperties {
 
             // port
             try {
-                port = Integer.parseInt(prop.getProperty(PROPERTY_PORT_DO_SERVER_1));
+                port = Integer.parseInt(prop.getProperty(PROPERTY_PORT));
             } catch (NumberFormatException e) {
                 logger.error("can't parse properties: can't read port", e);
                 return false;
             }
             logger.info("Port " + port);
 
-            // 3. Db
+            // Db
             urlForDb = prop.getProperty(PROPERTY_URL_FOR_SQL);
             logger.info("Url for postgresql" + urlForDb);
 
@@ -53,46 +45,8 @@ public class ForProperties {
         return true;
     }
 
-
-    private Boolean readIsRpi(String isRpiStr, Logger logger) {
-        if (null == isRpiStr) {
-            return null;
-        }
-
-        switch (isRpiStr) {
-            case "true":
-                return true;
-            case "false": isRpi = false;
-                return false;
-            default:
-                logger.error(
-                        "can't parse boolean parameter - isRpi. Valid values: \"true\" and \"false\". Actual value: "
-                                + isRpiStr);
-                return null;
-        }
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public Integer getPort() {
-        return port;
-    }
-
     public String getUrlForDb() {
         return urlForDb;
     }
 
-    public boolean getIsRpi() {
-        return isRpi;
-    }
-
-    public int getDeviceId() {
-        return deviceId;
-    }
-
-    public int getPeriod() {
-        return period;
-    }
 }
