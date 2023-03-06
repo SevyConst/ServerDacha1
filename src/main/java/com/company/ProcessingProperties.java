fc79bd9376c7954faeec89bbffd2e5cd4065f093
Konstantin Lopatko <const42@gmail.com>
2023-03-06 13:35:29 +0300

    fixed removing port from properties

--- src/main/java/com/company/ProcessingProperties.java
+++ src/main/java/com/company/ProcessingProperties.java
@@ -16,13 +16,11 @@ import java.util.Properties;
 public class ProcessingProperties {
     static Logger logger = LogManager.getLogger(ProcessingProperties.class.getName());
 
-    private static final String PROPERTY_PORT = "port";
     private static final String PROPERTY_URL_FOR_DB = "url_for_db";
     private static final String PROPERTY_PERIOD_SENT = "period_ping";  // in seconds
     private static final String PROPERTY_TELEGRAM_BOT_TOKEN = "telegram_bot_token";
     private static final String PROPERTY_CODE_WORD = "code_word";
 
-    private Integer port;
     private String urlForDb;
     private Integer periodPing;
     private String telegramBotToken;
@@ -33,7 +31,7 @@ public class ProcessingProperties {
 
         try(InputStream inputStream = new FileInputStream(pathConfig))  {
             prop.load(inputStream);
-            
+
             // Db
             urlForDb = prop.getProperty(PROPERTY_URL_FOR_DB);
             logger.info("Url for postgresql: " + urlForDb);
