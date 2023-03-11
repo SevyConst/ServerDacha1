2f835ff8d282b00aa9ee9f5c6b9762d89c9a7b21
Konstantin Lopatko <const42@gmail.com>
2023-03-11 12:56:42 +0300

    use @ConfigurationProperties

--- src/main/java/com/company/Application.java
+++ src/main/java/com/company/Application.java
@@ -1,25 +1,13 @@
 package com.company;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Application implements ApplicationRunner {
-
-    @Autowired
-    private ApplicationContext applicationContext;
+public class Application {
 
     public static void main(String[] args) {
 
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
-
     }
 }
--- src/main/java/com/company/ProcessingProperties.java
+++ src/main/java/com/company/ProcessingProperties.java
@@ -1,68 +1,45 @@
 package com.company;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 
 @ConfigurationProperties
 public class ProcessingProperties {
-    static Logger logger = LogManager.getLogger(ProcessingProperties.class.getName());
-
-    private static final String PROPERTY_URL_FOR_DB = "url_for_db";
-    private static final String PROPERTY_PERIOD_SENT = "period_ping";  // in seconds
-    private static final String PROPERTY_TELEGRAM_BOT_TOKEN = "telegram_bot_token";
-    private static final String PROPERTY_CODE_WORD = "code_word";
 
     private String urlForDb;
     private Integer periodPing;
     private String telegramBotToken;
     private String codeWord;
 
-    public ProcessingProperties(@Value("${pathConfig}") String pathConfig) {
-        Properties prop = new Properties();
-
-        try(InputStream inputStream = new FileInputStream(pathConfig))  {
-            prop.load(inputStream);
-
-            // Db
-            urlForDb = prop.getProperty(PROPERTY_URL_FOR_DB);
-            logger.info("Url for postgresql: " + urlForDb);
-
-            try {
-                periodPing = Integer.parseInt(prop.getProperty(PROPERTY_PERIOD_SENT));
-            } catch (NumberFormatException e) {
-                logger.error("can't parse properties: can't read period", e);
-                return;
-            }
-
-            telegramBotToken = prop.getProperty(PROPERTY_TELEGRAM_BOT_TOKEN);
-
-            codeWord = prop.getProperty(PROPERTY_CODE_WORD);
-
-        } catch(IOException e){
-            logger.error("can't read properties", e);
-        }
-    }
-
     public String getUrlForDb() {
         return urlForDb;
+    }
+
+    public void setUrlForDb(String urlForDb) {
+        this.urlForDb = urlForDb;
     }
 
     public Integer getPeriodPing() {
         return periodPing;
     }
 
-    String getTelegramBotToken() {
+    public void setPeriodPing(Integer periodPing) {
+        this.periodPing = periodPing;
+    }
+
+    public String getTelegramBotToken() {
         return telegramBotToken;
     }
 
-    String getCodeWord() { return codeWord; }
+    public void setTelegramBotToken(String telegramBotToken) {
+        this.telegramBotToken = telegramBotToken;
+    }
+
+    public String getCodeWord() {
+        return codeWord;
+    }
+
+    public void setCodeWord(String codeWord) {
+        this.codeWord = codeWord;
+    }
 }
